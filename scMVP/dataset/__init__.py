fedba1a476126e522c54f9ff6258ae9d5392b321
adamtongji <adam.tongji@gmail.com>
2020-09-11 14:21:49 +0800

    dataset clean

--- scMVP/dataset/__init__.py
+++ scMVP/dataset/__init__.py
@@ -1,72 +1,27 @@
-from scMVP.dataset.anndataset import AnnDatasetFromAnnData, DownloadableAnnDataset
 from scMVP.dataset.ATACDataset import ATACDataset
-from scMVP.dataset.brain_large import BrainLargeDataset
-from scMVP.dataset.cite_seq import CiteSeqDataset, CbmcDataset
-from scMVP.dataset.cortex import CortexDataset
-from scMVP.dataset.csv import CsvDataset, BreastCancerDataset, MouseOBDataset
 from scMVP.dataset.dataset import (
     GeneExpressionDataset,
     DownloadableDataset,
     CellMeasurement,
 )
-from scMVP.dataset.dataset10X import Dataset10X, BrainSmallDataset
 from scMVP.dataset.geneDataset import geneDataset
-from scMVP.dataset.hemato import HematoDataset
-from scMVP.dataset.loom import (
-    LoomDataset,
-    RetinaDataset,
-    PreFrontalCortexStarmapDataset,
-    FrontalCortexDropseqDataset,
-)
+
 from scMVP.dataset.scMVP_dataloader import scMVP_dataloader
-from scMVP.dataset.pbmc import PbmcDataset, PurifiedPBMCDataset
 from scMVP.dataset.pairedSeqDataset import pairedSeqDataset
-from scMVP.dataset.seqfish import SeqfishDataset
-from scMVP.dataset.seqfishplus import SeqFishPlusDataset
-from scMVP.dataset.smfish import SmfishDataset
+
 from scMVP.dataset.snareDataset import snareDataset
 from scMVP.dataset.scienceDataset import scienceDataset
-from scMVP.dataset.synthetic import (
-    SyntheticDataset,
-    SyntheticRandomDataset,
-    SyntheticDatasetCorr,
-    ZISyntheticDatasetCorr,
-)
+
 
 
 __all__ = [
-    "AnnDatasetFromAnnData",
     "ATACDataset",
-    "DownloadableAnnDataset",
-    "BrainLargeDataset",
-    "CiteSeqDataset",
-    "CbmcDataset",
     "CellMeasurement",
-    "CortexDataset",
-    "CsvDataset",
-    "BreastCancerDataset",
-    "MouseOBDataset",
     "GeneExpressionDataset",
-    "DownloadableDataset",
-    "Dataset10X",
-    "BrainSmallDataset",
+    "DownloadableDataset", # may remove in new version
     "geneDataset",
-    "HematoDataset",
-    "LoomDataset",
-    "RetinaDataset",
-    "FrontalCortexDropseqDataset",
     "pairedSeqDataset",
-    "PreFrontalCortexStarmapDataset",
-    "PbmcDataset",
-    "PurifiedPBMCDataset",
     "scienceDataset",
     "scMVP_dataloader",
-    "SeqfishDataset",
-    "SeqFishPlusDataset",
-    "SmfishDataset",
     "snareDataset",
-    "SyntheticDataset",
-    "SyntheticRandomDataset",
-    "SyntheticDatasetCorr",
-    "ZISyntheticDatasetCorr",
 ]
